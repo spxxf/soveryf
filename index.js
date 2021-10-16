30ad5e010b6cfb1e59742bacbbcb5166cd1822ae
anishbajpai91 <anishbajpai91@outlook.com>
2021-10-16 16:28:02 -0400

    finished bus routes
     still graph v1

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ busStops.addNode('Allison Road Classrooms/Science Buildings')
 busStops.addNode('Busch Student Center')
 busStops.addNode('Livingston Plaza')
 busStops.addNode('Livingston Student Center')
+busStops.addNode('Werblin Recreation Center')
+busStops.addNode('Stadium West Lot')
 
 x = new Array('RexB', 'RexL', 'EE', 'F')
 busStops.addEdge('Red Oak Lane', 'Lipman Hall', x)
@@ -38,17 +40,26 @@ busStops.addEdge('Gibbons', 'College Hall', x)
 x = new Array('RexB')
 busStops.addEdge('College Hall', 'Hill Center', x)
 
-x = new Array('RexB', 'A')
+x = new Array('RexB', 'A', 'C', 'B')
 busStops.addEdge('Hill Center', 'Allison Road Classrooms/Science Buildings', x)
 
 x = new Array('RexB')
 busStops.addEdge('Hill Center', 'Red Oak Lane', x)
 
-x = new Array('RexB', 'H')
+x = new Array('H', 'C')
+busStops.addEdge('Hill Center', 'Stadium West Lot', x)
+
+x = new Array('RexB', 'H', 'C')
 busStops.addEdge('Allison Road Classrooms/Science Buildings', 'Hill Center', x)
+
+x = new Array('A', 'B')
+busStops.addEdge('Allison Road Classrooms/Science Buildings', 'Busch Student Center')
 
 x = new Array('RexL')
 busStops.addEdge('Livingston Student Center', 'Red Oak Lane', x)
+
+x = new Array('B')
+busStops.addEdge('Livingston Student Center', 'Hill Center', x)
 
 x = new Array('EE')
 busStops.addEdge('College Hall', 'Socam', x)
@@ -62,10 +73,22 @@ busStops.addEdge('College Hall', 'Student Activities Center', x)
 x = new Array('EE')
 busStops.addEdge('Socam', 'Student Activities Center', x)
 
+x = new Array('EE')
+busStops.addEdge('Socam', 'Red Oak Lane')
+
 x = new Array('EE', 'F')
 busStops.addEdge('Student Activities Center', 'College Ave Student Center', x)
 
-x = new Array('EE', 'F')
+x = new Array('LX')
+busStops.addEdge('Student Activities Center', 'Livingstion Plaza', x)
+
+x = new Array('H')
+busStops.addEdge('Student Activities Center', 'Werblin Recreation Center', x)
+
+x = new Array('A')
+busStops.addEdge('Student Activities Center', 'Stadium West Lot', x)
+
+x = new Array('EE', 'F', 'LX', 'A', 'H')
 busStops.addEdge('College Ave Student Center', 'The Yard', x)
 
 x = new Array('EE')
@@ -74,7 +97,34 @@ busStops.addEdge('The Yard', 'Socam', x)
 x = new Array('F')
 busStops.addEdge('The Yard', 'Red Oak Lane', x)
 
+x = new Array('LX', 'H', 'A')
+busStops.addEdge('The Yard', 'Student Activities Center', x)
 
+x = new Array('RexL', 'LX', 'B')
+busStops.addEdge('Livingston Plaza', 'Livingston Student Center', x)
 
+x = new Array('B')
+busStops.addEdge('Busch Student Center', 'Livingston Plaza')
+
+x = new Array('H')
+busStops.addEdge('Busch Student Center', 'Allison Road Classrooms/Science Buildings', x)
+
+x = new Array('A')
+busStops.addEdge('Busch Student Center', 'Werblin Recreation Center', x)
+
+x = new Array('LX')
+busStops.addEdge('Livingston Student Center', 'College Ave Student Center')
+
+x = new Array('H')
+busStops.addEdge('Werblin Recreation Center', 'Busch Student Center', x)
+
+x = new Array('A')
+busStops.addEdge('Werblin Recreation Center', 'College Ave Student Center', x)
+
+x = new Array('H')
+busStops.addEdge('Stadium West Lot', 'College Ave Student Center', x)
+
+x = new Array('C', 'A')
+busStops.addEdge('Stadium West Lot', 'Hill Center', x)
 
 console.log(busStops.bfs())
